fe32df6f2e4511f93e1f4d759db8b0342dd2ec91
Bogar Martinez <bogar.cm@icloud.com>
2023-06-06 17:26:15 -0500

    math modules

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 31;
+let num2 = 2;
+let num3 = 22;
+let num4 = 9;
+let multiply = num1 * num2;
 
-// Write your code here
+let random =(Math.ceil(Math.random()*10)+1000);
+
+let mod = num3 % num4;
+
+let max = Math.max(1,2,3,20);
